d3e8c41445a8b6bef81fbdcd5ed3927b376a3fb1
Yordja <yordja@hotmail.com>
2016-03-27 00:17:07 +0000

    added sass style gem

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,10 @@
 class PagesController < ApplicationController
+  def index
+  end
 
-def index
-end
+  def contact
+  end
 
-
+  def about
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'pages/index'
+
+  get 'pages/contact'
+
+  get 'pages/about'
+
   root to: 'pages#index'
 
 end
