f44388e988e5956df727a430d5554c8b31385072
yordja@hotmail.com <yorjda@hotmail.com>
2016-10-23 15:14:15 +0000

    create article assosiation

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161022052151) do
+ActiveRecord::Schema.define(version: 20161023083442) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "models", force: :cascade do |t|
