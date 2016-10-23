029629f3f580358c8c8149939be1db57bb703555
yordja@hotmail.com <yorjda@hotmail.com>
2016-10-23 08:23:12 +0000

    scaffold crud articles

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   def index
+    @articles = Article.last(4)
   end
 
   def contact
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   
   devise_for :users, :controllers => { registrations: 'registrations'}
+  resources :articles
   root to: 'pages#index'
   get 'pages/contact'
   get 'pages/about'
